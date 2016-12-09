ecdc0af3dc4cea617c7c49fb007057476c451935
fatehuanchen <1540226204@qq.com>
2016-12-09 20:55:40 +0800

    2

--- src/JiSuan/Shui.java
+++ src/JiSuan/Shui.java
@@ -17,33 +17,33 @@ public class Shui {
 		}
 		if(salary>1500&&salary<=4500)
 		{
-			System.out.println("所需要缴纳的税费为："+(salary-1500)*0.1);
+			System.out.println("所需要缴纳的税费为："+((salary-1500)*0.1+1500*0.03));
 			
 		}
 		if(salary>4500&&salary<=9000)
 		{
-			System.out.println("所需要缴纳的税费为："+(salary-4500)*0.2);
+			System.out.println("所需要缴纳的税费为："+((salary-4500)*0.2+1500*0.03+3000*0.1));
 			
 		}
 		if(salary>9000&&salary<=35000)
 		{
-			System.out.println("所需要缴纳的税费为："+(salary-9000)*0.25);
+			System.out.println("所需要缴纳的税费为："+((salary-9000)*0.25+1500*0.03+3000*0.1+4500*0.2));
 			
 		}
 		
 		if(salary>35000&&salary<=55000)
 		{
-			System.out.println("所需要缴纳的税费为："+(salary-35000)*0.3);
+			System.out.println("所需要缴纳的税费为："+((salary-35000)*0.3)+1500*0.03+3000*0.1+4500*0.2+24000*0.25);
 			
 		}
 		if(salary>55000&&salary<=80000)
 		{
-			System.out.println("所需要缴纳的税费为："+(salary-55000)*0.35);
+			System.out.println("所需要缴纳的税费为："+(salary-55000)*0.35+1500*0.03+3000*0.1+4500*0.2+24000*0.25+2000*0.3);
 			
 		}
 		if(salary>80000)
 		{
-			System.out.println("所需要缴纳的税费为："+(salary-80000)*0.45);
+			System.out.println("所需要缴纳的税费为："+(salary-80000)*0.451500*0.03+3000*0.1+4500*0.2+24000*0.25+2000*0.3+25000*0.35);
 			
 		}
 		
